f967865beabef909b23bb0b5ddf0c3f52c01b85d
Michael Blockley <mikey@spotify.com>
2017-05-18 18:04:44 +0200

    fixed bugs

--- include/stw_types.hrl
+++ include/stw_types.hrl
@@ -1,7 +1,7 @@
 -record(stw_entry, {title :: binary(),
                     date :: calendar:datetime(),
                     author :: binary(),
-                    path :: binary()
+                    path :: stw_path()
                    }).
 -type stw_entry() :: #stw_entry{}.
 
